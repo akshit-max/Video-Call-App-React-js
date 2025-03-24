549d02c98c2216f8953c562b535deedd430da348
Rasheed khan <rktechnicalplus@gmail.com>
2025-03-24 18:19:00 +0530

    video call app
    complete project code

---
+++ src/Pages/About.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Layout from "../Components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout>
+      <div
+        className="content"
+        style={{
+          height: "75vh",
+          margin: "30px",
+        }}
+      >
+        <h1>About </h1>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+          fugit ipsum! Deleniti libero suscipit porro voluptatum itaque. Fuga
+          explicabo earum minus aperiam provident dignissimos deserunt
+          voluptatibus numquam distinctio veniam facere voluptates illum, quos
+          enim cumque ex quidem maiores repellendus expedita tempora, omnis
+          voluptatum odio. Suscipit reprehenderit nobis fugit perspiciatis
+          explicabo laudantium quo libero modi, quae, quia ad commodi magni
+          aspernatur nemo? Debitis ullam sint alias exercitationem quisquam
+          excepturi. Porro totam tempora sapiente rerum id eum assumenda
+          quibusdam. Ea fugiat aut dolorem cumque voluptatibus non minus.
+          Aliquid nostrum animi ea eaque ipsam reprehenderit eos pariatur fugit
+          voluptatum obcaecati architecto minus labore similique, in quam quo!
+          Hic quibusdam recusandae, eveniet impedit fugit numquam voluptate
+          officia mollitia? Impedit sapiente consectetur odit natus labore
+          quidem quasi omnis voluptas ab reiciendis totam veritatis unde nobis
+          aperiam iste velit, at fugiat minima cupiditate, dolore dignissimos
+          voluptate inventore temporibus? Consequatur, rem recusandae ea non
+          harum totam dicta laudantium voluptate ducimus blanditiis itaque
+          ratione reiciendis, assumenda et mollitia porro fugiat odio incidunt,
+          minus ipsa? Sed vel, aliquam commodi tempora consequatur, inventore
+          rem molestias, magnam iure soluta at nesciunt sit consequuntur!
+          Voluptates veritatis, molestiae est fugiat omnis reprehenderit dolores
+          quisquam beatae consequuntur tempore, mollitia corporis fuga, tenetur
+          at laboriosam.
+        </p>{" "}
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
